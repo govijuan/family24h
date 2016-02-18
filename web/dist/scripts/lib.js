e3798d8644d47a58dee60015aaaee854df514cd7
RenatoCRON <renato.cron@gmail.com>
2016-02-18 16:58:08 -0200

    versao dist do shin

--- web/dist/scripts/lib.js
+++ web/dist/scripts/lib.js
@@ -8,6 +8,12 @@ var user_id = getCookie("family_id");
 var api_key = getCookie("family_key");
 var user_data = {};
 
+
+/*TRADUTOR*/
+$.tr.dictionary(myDictionary);
+$.tr.language('pt_BR');
+var tr = $.tr.translator();
+
 /*----------------------------------------------------------------------------*\
     session handler
 \*----------------------------------------------------------------------------*/
@@ -51,7 +57,7 @@ function validateCookie(onsuccess, onerror) {
         });
     } else {
         if (onerror) {
-            onerror(data)
+            onerror()
         }
     }
 
@@ -101,7 +107,80 @@ function getCookie(name) {
 \*----------------------------------------------------------------------------*/
 function setCookie(id, api_key) {
     var exp = new Date(); //set new date object
-    var expires = exp.setTime(exp.getTime() + (1000 * 60 * 60 * 24 * 30)); //set it 30 days ahead
-    var idCookie = document.cookie = "family_id=" + escape(id) + "; path=/; expires=" + expires + ";";
-    var keyCookie = document.cookie = "family_key=" + escape(api_key) + "; path=/; expires=" + expires + ";";
+    var expires = new Date(exp.getTime() + (30 * 86400000)); //set it 30 days ahead
+    var idCookie = escape(id);
+    document.cookie = "family_id=" + escape(id) + "; path=/; expires=" + expires + ";";
+    var keyCookie = escape(api_key);
+    document.cookie = "family_key=" + escape(api_key) + "; path=/; expires=" + expires + ";";
+}
+
+
+/*----------------------------------------------------------------------------*\
+    send contact form
+\*----------------------------------------------------------------------------*/
+function sendContact() {
+    $("#contactForm .alert").html("");
+    $("#contactForm .alert").removeClass("alert-success");
+    $("#contactForm .alert").removeClass("alert-danger");
+
+  if ($("#contactForm #name").val() == ""){
+    $("#contactForm .alert").html(tr("Please inform your Name"));
+    $("#contactForm .alert").removeClass("alert-success");
+    $("#contactForm .alert").addClass("alert-danger");
+    return false;
+  }else if ($("#contactForm #email").val() == ""){
+    $("#contactForm .alert").html(tr("Please inform your Email"));
+    $("#contactForm .alert").removeClass("alert-success");
+    $("#contactForm .alert").addClass("alert-danger");
+    return false;
+  }else if (!validateEmail($("#contactForm #email").val())){
+    $("#contactForm .alert").html(tr("Email invalid"));
+    $("#contactForm .alert").removeClass("alert-success");
+    $("#contactForm .alert").addClass("alert-danger");
+    return false;
+  }else if ($("#contactForm #message").val() == ""){
+    $("#contactForm .alert").html(tr("Please inform your Message"));
+    $("#contactForm .alert").removeClass("alert-success");
+    $("#contactForm .alert").addClass("alert-danger");
+    return false;
+  }
+  
+  var contact_url = "http://contato.family24h.com/mail";
+  
+  var request = "";
+  
+  request += "name=" + escape($("#contactForm #name").val());
+  request += "&email=" + escape($("#contactForm #email").val());
+  request += "&message=" + escape($("#contactForm #message").val());
+  
+  $.ajax({
+      url: contact_url,
+      type: "POST",
+      data: request,
+      crossDomain: true,
+      complete: function(e, xhr, settings){
+          if(e.status === 200 || e.status === 202){
+            var retorno = JSON.parse(e.responseText);
+            if (retorno.success == "yes"){
+              $("#contactForm .alert").html(tr("Your message has been sent. Thank You."));
+              $("#contactForm .alert").addClass("alert-success");
+              $("#contactForm .alert").removeClass("alert-danger");
+            }else{
+              $("#contactForm .alert").html(tr("Error sending message. Please try again later."));
+              $("#contactForm .alert").removeClass("alert-success");
+              $("#contactForm .alert").addClass("alert-danger");
+            }
+          }
+      },
+      error: function(xhr, status, error){
+          console.log(xhr);
+          $("#contactForm .alert").html(tr("Error sending message. Please try again later.") + " (" + xhr.status + ")");
+          $("#contactForm .alert").removeClass("alert-success");
+          $("#contactForm .alert").addClass("alert-danger");
+      }
+  });
+}
+function validateEmail(email) { 
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
 }
